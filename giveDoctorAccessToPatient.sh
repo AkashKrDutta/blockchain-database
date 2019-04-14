41bafacf57cadc10e55f01e284f772ab22509df2
AKASH KUMAR DUTTA <akashdut@cse.iitk.ac.in>
2019-04-15 01:39:03 +0530

    Basic working of doctor and patient algorithm

---
+++ giveDoctorAccessToPatient.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Gives read and write accesses doctor to a user
+
+if (( $# != 4 ))
+then
+    echo "Usage: ./giveDoctorAccessToPatient <root_uname> <db_name> <doc_id> <user_id>"
+    exit
+fi
+
+cat << EOF | mysql --user=$1 --password
+USE $2;
+CREATE VIEW $3_$4 as SELECT * FROM $4 WHERE DoctorName like '$3%';
+GRANT SELECT, INSERT ON $2.$3_$4 TO '$3'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+
